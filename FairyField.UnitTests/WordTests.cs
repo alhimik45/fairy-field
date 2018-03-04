7ef528c06662c25a7b79be5353614631c7bc2874
alhimik45 <mail@alhimik.me>
2018-03-05 01:36:46 +0700

    not create null words

--- FairyField.UnitTests/WordTests.cs
+++ FairyField.UnitTests/WordTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace FairyField.UnitTests
@@ -9,8 +10,15 @@ namespace FairyField.UnitTests
         [Test]
         public void Word_AfterCreating_HasClosedLetters()
         {
-            var word = new Word();
+            var word = new Word("asd");
             word.HaveClosedLetters.Should().BeTrue();
+        }
+
+        [Test]
+        public void CreateWord_ThrowArgumentException_IfWordIsNull()
+        {
+            Action act = () => new Word(null);
+            act.Should().Throw<ArgumentException>();
         }
     }
 }
--- FairyField/Word.cs
+++ FairyField/Word.cs
@@ -1,7 +1,17 @@
-﻿namespace FairyField
+﻿using System;
+
+namespace FairyField
 {
     public class Word
     {
         public bool HaveClosedLetters => true;
+
+        public Word(string word)
+        {
+            if(word == null)
+            {
+                throw new ArgumentException();
+            }
+        }
     }
 }
