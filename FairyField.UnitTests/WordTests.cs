e14bde2971793d151ebb43fcf6c02e693d099eef
alhimik45 <mail@alhimik.me>
2018-03-05 01:32:44 +0700

    start writing Word

---
+++ FairyField.UnitTests/WordTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FairyField.UnitTests
+{
+    [TestFixture]
+    public class WordTests
+    {
+        [Test]
+        public void Word_AfterCreating_HasClosedLetters()
+        {
+            var word = new Word();
+            word.HaveClosedLetters.Should().BeTrue();
+        }
+    }
+}
