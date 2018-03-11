43313d4349bf314db991f9e322d66acf6fed4726
alhimik45 <mail@alhimik.me>
2018-03-11 17:16:52 +0700

    LetterAction

---
+++ FairyField.UnitTests/DrumActions/LetterActionTests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using FairyField.DrumActions;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FairyField.UnitTests.DrumActions
+{
+    [TestFixture]
+    public class LetterActionTests
+    {
+        [Test]
+        public void LetterAction_OpensLetter()
+        {
+            var state = new GameState(new Word("a"));
+            var input = Substitute.For<TextReader>();
+            input.ReadLine().Returns("1");
+            var letterAction = new LetterAction(input, Substitute.For<TextWriter>());
+            letterAction.Act(state);
+            state.CurrentWord.View.Should().Be("a");
+        }
+
+        [Test]
+        public void LetterAction_WaitsCorrectLetterIndex()
+        {
+            var state = new GameState(new Word("a"));
+            var input = Substitute.For<TextReader>();
+            input.ReadLine().Returns("0", "-13", "445", "sdsd","1");
+            var letterAction = new LetterAction(input, Substitute.For<TextWriter>());
+            letterAction.Act(state);
+            state.CurrentWord.View.Should().Be("a");
+            input.Received(5).ReadLine();
+        }
+    }
+}
