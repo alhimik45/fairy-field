fd486273361e3812287768a238a0efe69b506c17
alhimik45 <mail@alhimik.me>
2018-03-11 15:35:48 +0700

    add GameState

---
+++ FairyField.UnitTests/GameStateTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FairyField.UnitTests
+{
+    [TestFixture]
+    public class GameStateTests
+    {
+        [Test]
+        public void GameState_AfterCreating_HasZeroScore()
+        {
+            var state = new GameState(new Word("a"));
+            state.Scores.Should().Be(0);
+        }
+    }
+}
---
+++ FairyField/GameState.cs
@@ -0,0 +1,13 @@
+﻿namespace FairyField
+{
+    public class GameState
+    {
+        public Word CurrentWord { get; set; }
+        public int Scores { get; set; } = 0;
+
+        public GameState(Word currentWord)
+        {
+            CurrentWord = currentWord;
+        }
+    }
+}
