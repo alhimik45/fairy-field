615690b4ecc60bb2faac87140cbcf41d9d7b55f9
alhimik45 <mail@alhimik.me>
2018-03-11 16:45:38 +0700

    Bankrupt action

---
+++ FairyField.UnitTests/DrumActions/BankruptTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using FairyField.DrumActions;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FairyField.UnitTests.DrumActions
+{
+    [TestFixture]
+    public class BankruptTests
+    {
+        [Test]
+        public void BankruptAction_SetsScoresToZero()
+        {
+            var state = new GameState(new Word("a")) {Scores = 42};
+            var bankruptAction = new BankruptAction(Substitute.For<TextWriter>());
+            bankruptAction.Act(state);
+            state.Scores.Should().Be(0);
+        }
+    }
+}
